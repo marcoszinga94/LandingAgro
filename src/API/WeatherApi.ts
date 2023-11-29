06f35c2b995689c69825a6569a6225bd077561fd
Zinike <marcoszinga_1994@hotmail.com>
2023-11-29 19:11:06 -0300

    commit

--- src/API/WeatherApi.ts
+++ src/API/WeatherApi.ts
@@ -1,86 +1,53 @@
-// script.ts
-document.addEventListener("DOMContentLoaded", function () {
-  const apiKey: string = "7bb40db53e202dc7da26d5404411e9c4";
-  let locationKey: string | null = null;
+// APIKEY = 7bb40db53e202dc7da26d5404411e9c4
 
-  const weatherContainer: HTMLElement | null =
-    document.getElementById("weather-container");
+document.addEventListener("DOMContentLoaded", async () => {
+  const apiKey = "7bb40db53e202dc7da26d5404411e9c4";
+  const weatherContainer = document.getElementById("weather-container");
 
-  interface WeatherData {
-    name: string;
-    main: {
-      temp: number;
-    };
-    weather: [
-      {
-        description: string;
-      }
-    ];
-  }
+  const getUserLocation = () =>
+    new Promise<GeolocationPosition>((resolve, reject) => {
+      navigator.geolocation.getCurrentPosition(resolve, reject);
+    });
 
-  const getUserLocation = (): void => {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(
-        async (position) => {
-          const { latitude, longitude } = position.coords;
-          locationKey = await getLocationKeyByCoordinates(latitude, longitude);
+  const getLocationKeyByCoordinates = async (
+    latitude: number,
+    longitude: number
+  ) => `${latitude},${longitude}`;
 
-          if (locationKey) {
-            getWeatherData();
-          } else {
-            console.error("No se pudo obtener el código de ubicación.");
-          }
-        },
-        (error) => {
-          console.error("Error al obtener la ubicación del usuario", error);
-        }
-      );
-    } else {
-      console.error("Geolocalización no compatible en este navegador.");
-    }
-  };
-
-  const getWeatherData = async (): Promise<void> => {
+  const getWeatherData = async () => {
     try {
-      if (!locationKey) {
-        console.error("No hay código de ubicación disponible.");
-        return;
-      }
+      const { coords } = (await getUserLocation()) as GeolocationPosition;
+      const locationKey = await getLocationKeyByCoordinates(
+        coords.latitude,
+        coords.longitude
+      );
 
       const response = await fetch(
-        `https://api.openweathermap.org/data/2.5/weather?lat=${locationKey}&lon=${locationKey}&appid=${apiKey}&units=metric`
+        `hhttps://api.openweathermap.org/data/2.5/weather?lat=-33.44&lon=-66.55&appid=7bb40db53e202dc7da26d5404411e9c4&units=metric&lang=es`
       );
-      const data: WeatherData = await response.json();
+      const data = await response.json();
+
       displayWeather(data);
     } catch (error) {
       console.error("Error al obtener datos meteorológicos", error);
     }
   };
 
-  const getLocationKeyByCoordinates = async (
-    latitude: number,
-    longitude: number
-  ): Promise<string | null> => {
-    try {
-      return `${latitude},${longitude}`;
-    } catch (error) {
-      console.error("Error al obtener las coordenadas", error);
-      return null;
-    }
-  };
-
-  const displayWeather = (weatherData: WeatherData): void => {
+  const displayWeather = (weatherData: any) => {
     if (weatherContainer) {
-      const weatherHtml: string = `
-        <div>
-          <h3 class="text-xl">${weatherData.name}</h3>
-          <p class="text-lg">${weatherData.main.temp}°C</p>
-          <p class="text-sm">${weatherData.weather[0].description}</p>
+      const weatherHtml = `
+        <div class="flex flex-row">
+          <img src"${weatherData.weather[0].icon}"
+          <div class="flex flex-col">
+            <h3 class="text-xl">Ubicacion: ${weatherData.name}</h3>
+            <p class="text-lg">Temperatura: ${weatherData.main.temp}°C</p>
+            <p class="text-sm">Tiempo: ${weatherData.weather[0].description}</p>
+          </div>
         </div>
       `;
       weatherContainer.innerHTML = weatherHtml;
     }
   };
 
-  getUserLocation();
+  getWeatherData();
 });
