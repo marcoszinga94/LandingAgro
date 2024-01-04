57400dabb858f2744c343091c2c06205e650fd5a
Zinike <marcoszinga94@gmail.com>
2024-01-04 01:29:42 -0300

    commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import partytown from "@astrojs/partytown";
+import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
